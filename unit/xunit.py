4f6171d55259c7577eaf20db118ecb9849515a75
kanghyki <kanghyki@gmail.com>
2025-07-05 20:55:46 +0900

    test: add testFailedResult

--- unit/xunit.py
+++ unit/xunit.py
@@ -1,3 +1,13 @@
+class TestResult():
+    def __init__(self):
+        self.runCount = 0
+
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+
+    def summary(self):
+        return f"{self.runCount} run, 0 failed"
+
 class TestCase:
     def __init__(self, name):
         self.name = name
@@ -9,10 +19,13 @@ class TestCase:
         pass
 
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
 
 class WasRun(TestCase):
     def __init__(self, name):
@@ -24,6 +37,9 @@ class WasRun(TestCase):
     def testMethod(self):
         self.wasRun = True
         self.log = self.log + "testMethod "
+
+    def testBrokenMethod(self):
+        raise Exception
 
     def setUp(self):
         self.wasRun = None
@@ -45,5 +61,18 @@ class TestCaseTest(TestCase):
         test.run()
         assert("setUp testMethod tearDown " == test.log)
 
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
+
+
 TestCaseTest("testRunning").run()
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+TestCaseTest("testFailedResult").run()
